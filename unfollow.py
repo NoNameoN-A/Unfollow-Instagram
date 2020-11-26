dfba41d7f5628e6ea3d79d090ae62b0b29038815
NoNameoN <nonameon@protonmail.ch>
2020-11-26 11:24:21 +0100

    Update unfollow.py

--- unfollow.py
+++ unfollow.py
@@ -6,36 +6,31 @@ import re
 import pandas as pd
 import requests
 
-"""chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-"""
 chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/home/nonameon/Documents/chromedriver',chrome_options=chrome_options)
+driver = webdriver.Chrome('/your/path/chromedriver',chrome_options=chrome_options) #If you have Windows use .exe
 
+USERNAME = "YOUR_USERNAME" #It must be changed
+PASSWORD = "YOUR_PASSWORD" #It must be changed
 
 print("Browser opened!")
 driver.get("https://www.instagram.com/")
 sleep(20)
 print("Loading...")
 driver.find_element_by_xpath("//input[@name=\"username\"]")\
-    .send_keys("YOUR_USERNAME")
+    .send_keys(USERNAME) 
 sleep(5)
 driver.find_element_by_xpath("//input[@name=\"password\"]")\
-    .send_keys("YOUR_PASSWORD")
+    .send_keys(PASSWORD) 
 sleep(5)
 driver.find_element_by_xpath('//button[@type="submit"]')\
     .click()
 sleep(20)
 print("Logged!")
-url = "https://www.instagram.com/curiosita_losapeviche_ita"
-driver.get(url)
+url = "https://www.instagram.com/{}"
+driver.get(url.format(USERNAME))
 sleep(20)
 following = driver.find_element_by_partial_link_text("following")
 following.click()
@@ -43,10 +38,10 @@ sleep(20)
 unfollowed = 0
 fBody  = driver.find_element_by_xpath("//div[@class='isgrP']")
 sleep(10)
-whiteList = ["curiosita_losapeviche_ita", "else"]
+whiteList = ["curiosita_losapeviche_ita", "else"] #WHITE-LIST IF YOU WANT
 
 
-for x in range(150):
+for x in range(150): #YOU CAN CHANGE THIS VALUE
     try:
         driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody) #scroll
         sleep(1)
@@ -64,8 +59,8 @@ print("%d users" % len(users))
 for user in users:
     jump = randrange(10)
     iterator += 1
-    nome = user.text
-    if jump <= 7 and not nome in whiteList: #unfollow
+    name = user.text
+    if jump <= 7 and not name in whiteList: #unfollow
         user.find_element_by_xpath(unfollowPath.format(iterator)).click()
         sleep(5)
         driver.find_element_by_xpath('//button[text()="Unfollow"]').click()
@@ -73,9 +68,9 @@ for user in users:
         sleep(5)
         print("%d users unfollowed" % unfollowed)
         usernUnfollowed = "{} unfollowed"
-        print(usernUnfollowed.format(nome))
+        print(usernUnfollowed.format(name))
         wait = randrange(100, 300)
         sleep(wait)
-    else:
+    else: #not unfollowed
         toPrint = "Impossible unfollow: {}"
-        print(toPrint.format(nome))
+        print(toPrint.format(name))
